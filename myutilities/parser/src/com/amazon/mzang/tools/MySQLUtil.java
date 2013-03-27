a5c7d81ee61c30e993e37233dd56c886010452e5
DeepNightTwo <DeepNightTwo@gmail.com>
2013-03-27 12:35:08 +0800

    asdfasdf

--- myutilities/parser/src/com/amazon/mzang/tools/MySQLUtil.java
+++ myutilities/parser/src/com/amazon/mzang/tools/MySQLUtil.java
@@ -9,62 +9,64 @@ import java.util.Map;
 
 public class MySQLUtil {
 
-	public static final String US_DB_Serv = "jdbc:mysql://oih-mysql-na.db.amazon.com/";
-	public static final String JP_DB_Serv = "jdbc:mysql://oih-mysql-fe.db.amazon.com/";
-	public static final String CN_DB_Serv = "jdbc:mysql://oih-mysql-cn.db.amazon.com/";
-	public static final String EU_DB_Serv = "jdbc:mysql://oih-mysql-eu.db.amazon.com/";
+    public static final String US_DB_Serv = "jdbc:mysql://oih-mysql-na.db.amazon.com/";
+    public static final String JP_DB_Serv = "jdbc:mysql://oih-mysql-fe.db.amazon.com/";
+    public static final String CN_DB_Serv = "jdbc:mysql://oih-mysql-cn.db.amazon.com/";
+    public static final String EU_DB_Serv = "jdbc:mysql://oih-mysql-eu.db.amazon.com/";
 
-	public static ResultSet executeSQL(String dbServ, String db, String sql) {
-		String driver = "com.mysql.jdbc.Driver";
+    private static Connection conn = null;
 
-		String url = dbServ + db;
+    public static ResultSet executeSQL(String dbServ, String db, String sql) {
+        String driver = "com.mysql.jdbc.Driver";
 
-		String user = "oihadmin";
+        String url = dbServ + db;
 
-		String password = "";
+        String user = "oihadmin";
 
-		try {
+        String password = "";
 
-			Class.forName(driver);
+        try {
 
-			Connection conn = DriverManager.getConnection(url, user, password);
+            Class.forName(driver);
 
-			if (!conn.isClosed())
+            if (conn == null)
+                conn = DriverManager.getConnection(url, user, password);
 
-				System.out.println("Succeeded connecting to the Database!");
+            // if (!conn.isClosed())
+            // System.out.println("Succeeded connecting to the Database!");
 
-			Statement statement = conn.createStatement();
-			ResultSet rs = statement.executeQuery(sql);
-			return rs;
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		return null;
-	}
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            return rs;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 
-	public static ResultSet executeSQLUsingIOG(int iog, String db, String sql) {
-		String dbServ = iog2SqlSvrUrl.get(iog);
-		if (dbServ == null) {
-			dbServ = EU_DB_Serv;
-		}
-		return executeSQL(dbServ, db, sql);
-	}
+    public static ResultSet executeSQLUsingIOG(int iog, String db, String sql) {
+        String dbServ = iog2SqlSvrUrl.get(iog);
+        if (dbServ == null) {
+            dbServ = EU_DB_Serv;
+        }
+        return executeSQL(dbServ, db, sql);
+    }
 
-	private static Map<Integer, String> iog2SqlSvrUrl = new HashMap<Integer, String>();
+    private static Map<Integer, String> iog2SqlSvrUrl = new HashMap<Integer, String>();
 
-	static {
-		iog2SqlSvrUrl.put(1, US_DB_Serv);
-		iog2SqlSvrUrl.put(2, US_DB_Serv);
-		iog2SqlSvrUrl.put(101, US_DB_Serv);
+    static {
+        iog2SqlSvrUrl.put(1, US_DB_Serv);
+        iog2SqlSvrUrl.put(2, US_DB_Serv);
+        iog2SqlSvrUrl.put(101, US_DB_Serv);
 
-		iog2SqlSvrUrl.put(11, US_DB_Serv);
-		iog2SqlSvrUrl.put(77, US_DB_Serv);
+        iog2SqlSvrUrl.put(11, US_DB_Serv);
+        iog2SqlSvrUrl.put(77, US_DB_Serv);
 
-		iog2SqlSvrUrl.put(10, JP_DB_Serv);
-		iog2SqlSvrUrl.put(33, JP_DB_Serv);
+        iog2SqlSvrUrl.put(10, JP_DB_Serv);
+        iog2SqlSvrUrl.put(33, JP_DB_Serv);
 
-		iog2SqlSvrUrl.put(70, CN_DB_Serv);
-		iog2SqlSvrUrl.put(71, CN_DB_Serv);
-	}
+        iog2SqlSvrUrl.put(70, CN_DB_Serv);
+        iog2SqlSvrUrl.put(71, CN_DB_Serv);
+    }
 
 }
